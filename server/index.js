9724995cae5f40ea1d8d2eab7e2e86845662528a
Vincent Castelli <vincentsalvatorecastelli@gmail.com>
2018-06-14 15:38:06 -0700

    Rendering photo service through proxy.

--- server/index.js
+++ server/index.js
@@ -6,10 +6,9 @@ const path = require('path');
 const app = express();
 
 app.use(bodyParser.json());
-app.use('/', express.static(path.join(__dirname, '../public')));
+app.use('/restaurant/:restaurantId', express.static(path.join(__dirname, '../public')));
 
-app.use('/photos/photoBundle.js', proxy('http://localhost:3004'));
-
+app.use('/photos', proxy('http://localhost:3004'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, console.log(`Listening on ${port}`));
