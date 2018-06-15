d86e708ecc1eb1cd8aeb51c9fb91b99b3452d4fb
Vincent Castelli <vincentsalvatorecastelli@gmail.com>
2018-06-15 08:49:04 -0700

    Added Overviews, Menus and Reviews to proxy.

--- server/index.js
+++ server/index.js
@@ -8,7 +8,10 @@ const app = express();
 app.use(bodyParser.json());
 app.use('/restaurant/:restaurantId', express.static(path.join(__dirname, '../public')));
 
+app.use('/reviews', proxy('http://localhost:3002'));
+app.use('/overviews', proxy('http://localhost:3003'));
 app.use('/photos', proxy('http://localhost:3004'));
+app.use('/menus', proxy('http://localhost:3005'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, console.log(`Listening on ${port}`));
