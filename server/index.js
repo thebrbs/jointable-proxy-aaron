9db72a5b661916a9391bcfb05e7976a7fd906a4f
Vincent Castelli <vincentsalvatorecastelli@gmail.com>
2018-06-17 22:26:28 -0700

    Modifications for deployment.

--- server/index.js
+++ server/index.js
@@ -8,11 +8,11 @@ const app = express();
 app.use(bodyParser.json());
 app.use('/restaurant/:restaurantId', express.static(path.join(__dirname, '../public')));
 
-app.use('/reservations', proxy('http://localhost:3001'));
-app.use('/reviews', proxy('http://localhost:3002'));
-app.use('/overviews', proxy('http://localhost:3003'));
-app.use('/photos', proxy('http://localhost:3004'));
-app.use('/menus', proxy('http://localhost:3005'));
+app.use('/reservations', proxy('http://cavareservations.us-west-2.elasticbeanstalk.com/'));
+app.use('/overviews', proxy('http://cavatableoverviews-env.5sves92ky9.us-west-1.elasticbeanstalk.com/'));
+app.use('/photos', proxy('CavatablePhotos-env-1.23pb2q7mim.us-east-2.elasticbeanstalk.com/'));
+app.use('/menus', proxy('http://cavatablemenus-env.5sves92ky9.us-west-1.elasticbeanstalk.com/'));
+app.use('/reviews', proxy('http://CavatableFec-env.psexkp69kr.us-west-1.elasticbeanstalk.com'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, console.log(`Listening on ${port}`));
